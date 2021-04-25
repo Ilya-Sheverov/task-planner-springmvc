7526659413c9245107a722df6a88626e9b9d0248
Ilya-Sheverov <ilya.sheverov@gmail.com>
2021-04-25 23:37:59 +0300

    Добавлены батники на создание и удалени БД.

---
+++ scripts/sql/task-planner-db-drop.bat
@@ -0,0 +1,17 @@
+chcp 65001
+ECHO OFF
+
+SET /P PGUSER="Enter the user to connect to the database[postgres]:"
+IF "%PGUSER%"=="" (
+SET PGUSER=postgres
+)
+SET /P PGHOST="Enter the host to connect to the database[127.0.0.1]:"
+IF "%PGHOST%"=="" (
+SET PGHOST=127.0.0.1
+)
+SET /P PGPORT="Enter the port to connect to the database[5432]:"
+IF "%PGPORT%"=="" (
+SET PGPORT=5432
+)
+psql -U %PGUSER%  -h %PGHOST% -p %PGPORT% -f "%CD%/psql-db-drop-script.sql" -e
+pause
