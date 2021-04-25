26f075e80f24c362e684988bbb76220dc487fec9
Ilya-Sheverov <ilya.sheverov@gmail.com>
2021-04-25 17:00:54 +0300

    Добавлены скрипты на удаление базы данных и её таблиц.

---
+++ scripts/sql/psql-tables-drop-script.sql
@@ -0,0 +1,9 @@
+DROP TABLE if exists assignee_groups CASCADE;
+
+DROP TABLE if exists statuses CASCADE;
+
+DROP TABLE if exists statuses_localized CASCADE;
+
+DROP TABLE if exists persons CASCADE;
+
+DROP TABLE if exists tasks CASCADE;
